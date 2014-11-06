85451f6f09a2b2cb9c8bf74fb5ceaa82d3f684d7
rodica-andronache <rodik_andronache@yahoo.com>
2014-11-06 16:55:12 +0200

    wp.org fixes and fixed fatall error from wp product review

--- single.php
+++ single.php
@@ -1,33 +1,33 @@
-<?php get_header(); ?>
-
-<section id="main-content" class="clearfix">
-    <section id="main-content-inner" class="container">
-    <div class="article-container post">
-        <?php while ( have_posts() ) : the_post(); ?>
-        <header class="clearfix">
-            <div class="article-details">
-                <h1 class="title"><?php the_title(); ?></h1>
-                <div class="meta">
-                    <span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span>
-                </div><!-- end .meta -->
-            </div><!-- end .article-details -->  
-        </header>
-           
-        <article <?php post_class("clearfix"); ?>> <?php the_content(); ?> <?php the_tags('Tags: ', ', ', '<br />'); ?> </article>
-
-        <?php get_template_part("templates/bookrev_review_wrap_up_template"); ?>
-
-        <?php wp_link_pages(); ?>
-
-        <?php endwhile; ?>
-            
-        <?php comments_template(); ?>
-
-    </div><!-- end .article-container -->
-
-    <?php get_sidebar(); ?>
-
-    </section><!-- end .main-content-inner -->
-</section><!-- end #main-content -->
-
+<?php get_header(); ?>
+
+<section id="main-content" class="clearfix">
+    <section id="main-content-inner" class="container">
+    <div class="article-container post">
+        <?php while ( have_posts() ) : the_post(); ?>
+        <header class="clearfix">
+            <div class="article-details">
+                <h1 class="title"><?php the_title(); ?></h1>
+                <div class="meta">
+                    <span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+                </div><!-- end .meta -->
+            </div><!-- end .article-details -->  
+        </header>
+           
+        <article <?php post_class("clearfix"); ?>> <?php the_content(); ?> <?php the_tags( __( 'Tags: ', 'book-rev-lite' ), __( ', ', 'book-rev-lite' ), '<br />' ); ?> </article>
+
+        <?php get_template_part("templates/bookrev_review_wrap_up_template"); ?>
+
+        <?php wp_link_pages(); ?>
+
+        <?php endwhile; ?>
+            
+        <?php comments_template(); ?>
+
+    </div><!-- end .article-container -->
+
+    <?php get_sidebar(); ?>
+
+    </section><!-- end .main-content-inner -->
+</section><!-- end #main-content -->
+
 <?php get_footer(); ?>
--- templates/bookrev_highlight_day_template.php
+++ templates/bookrev_highlight_day_template.php
@@ -1,37 +1,37 @@
-        <section id="highlight-day" class="newsblock clearfix">
-            <header>
-                <h2><?php book_rev_lite_string_template_category_replace('mp_hotd_title', 'mp_hotd_cat', 'category_selected'); ?></h2>
-            </header>
-            <?php
-            $sticky = get_option( 'sticky_posts' );
-            $query = new WP_Query(array('cat' => get_theme_mod('mp_hotd_cat'),'posts_per_page' => 1,'post__in' => $sticky));
-            if(isset($sticky[0])):
-                if($query->have_posts()) :
-                    while($query->have_posts()):
-                        $query->the_post(); ?>
-
-            <div class="highlight-inner">
-                <div class="featured-image">
-                    <img src="<?php book_rev_lite_get_post_feat_image_url(get_the_ID()); ?>">
-                    <span class="comments">
-                        <i class="fa fa-comments"></i>
-                        <?php comments_number("0", "1", "%"); ?>
-                    </span><!-- end .comments -->
-                </div><!-- end .featured-image -->
-
-                <div class="article-details">
-                    <h2 class="title"><a href="<?php echo get_permalink(get_the_ID()); ?>"><?php the_title(); ?></a></h2>
-                    <div class="meta">
-                        <span class="categ"><?php book_rev_lite_get_post_categories(get_the_ID(), 1); ?></span>
-                        <span class="date"><?php echo the_date(); ?></span>
-                    </div><!-- end .meta -->
-                </div><!-- end .article-details -->
-
-                <p><?php book_rev_lite_get_limited_content(get_the_id(), 450, '...'); ?></p>
-            </div><!-- end .highlight-inner -->
-
-            <?php endwhile;endif;endif; ?>
-
-
-
+        <section id="highlight-day" class="newsblock clearfix">
+            <header>
+                <h2><?php book_rev_lite_string_template_category_replace('mp_hotd_title', 'mp_hotd_cat', 'category_selected'); ?></h2>
+            </header>
+            <?php
+            $sticky = get_option( 'sticky_posts' );
+            $query = new WP_Query(array('cat' => get_theme_mod('mp_hotd_cat'),'posts_per_page' => 1,'post__in' => $sticky));
+            if(isset($sticky[0])):
+                if($query->have_posts()) :
+                    while($query->have_posts()):
+                        $query->the_post(); ?>
+
+            <div class="highlight-inner">
+                <div class="featured-image">
+                    <img src="<?php book_rev_lite_get_post_feat_image_url(get_the_ID()); ?>">
+                    <span class="comments">
+                        <i class="fa fa-comments"></i>
+                        <?php comments_number("0", "1", "%"); ?>
+                    </span><!-- end .comments -->
+                </div><!-- end .featured-image -->
+
+                <div class="article-details">
+                    <h2 class="title"><a href="<?php echo get_permalink(get_the_ID()); ?>"><?php the_title(); ?></a></h2>
+                    <div class="meta">
+                        <span class="categ"><?php book_rev_lite_get_post_categories(get_the_ID(), 1); ?></span>
+                        <span class="date"><?php echo the_date(); ?></span>
+                    </div><!-- end .meta -->
+                </div><!-- end .article-details -->
+
+                <p><?php book_rev_lite_get_limited_content(get_the_id(), 450, '...'); ?></p>
+            </div><!-- end .highlight-inner -->
+            
+            <?php endwhile;endif;endif; ?>
+
+
+
         </section><!-- end #highlight-day -->
